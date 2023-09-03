b983b1d9a976939e067752354664c6e3e60b6dea
Juan Valdebenito <valdebenitovaldiviajuan@gmail.com>
2023-09-02 20:04:06 -0400

    feat: add component card.tsx

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,11 @@
+import s from "./Card.module.css";
+
+const Card = () => {
+  return (
+    <div className={s.containerCard}>
+      <h1 className={s.titleCard}>PERFILES DE GITHUB</h1>
+    </div>
+  );
+};
+
+export default Card;
